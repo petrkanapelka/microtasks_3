f9490d683e63e427cae6513d5e1a13dce1b54747
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-16 23:31:39 +0300

    feat: add navlinks

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { PageTwo } from "./components/pages/PageTwo";
 import styles from "./components/Site.module.css";
 import { Error404 } from "./components/pages/Error404";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 
 function App() {
@@ -14,13 +15,22 @@ function App() {
             <div className={styles.body}>
                 <div className={styles.nav}>
                     <div>
-                        <NavLink to={'/page1'}>ADIDAS</NavLink>
+                        <NavLink
+                            to={'/page1'}
+                            className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}
+                        >ADIDAS</NavLink>
                     </div>
                     <div>
-                        <NavLink to={'/page2'}>PUMA</NavLink>
+                        <NavLink
+                            to={'/page2'}
+                            className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}
+                        >PUMA</NavLink>
                     </div>
                     <div>
-                        <NavLink to={'/page3'}>ABIBAS</NavLink>
+                        <NavLink
+                            to={'/page3'}
+                            className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}
+                        >ABIBAS</NavLink>
                     </div>
 
                 </div>
@@ -42,5 +52,20 @@ function App() {
     );
 }
 
+const NavWrapper = styled.div`
+    margin-left: 10px;
+    font-size: 20px;
+    &>a{
+        text-decoration: none;
+        color: #1e3786;
+    }
+    &>a.active{
+        text-decoration: none;
+        color: #03eaff;
+    }
+    &>a:hover{
+        color: steelblue;
+    }
+`
 
 export default App;
