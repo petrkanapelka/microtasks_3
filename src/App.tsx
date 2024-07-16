a28054edba023380d5c124639862f7b0e1a04aee
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-16 23:05:42 +0300

    feat: add NavLinks

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { PageThree } from "./components/pages/PageThree";
 import { PageTwo } from "./components/pages/PageTwo";
 import styles from "./components/Site.module.css";
 import { Error404 } from "./components/pages/Error404";
+import { NavLink } from "react-router-dom";
 
 
 function App() {
@@ -12,15 +13,26 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <div>
+                        <NavLink to={'/page1'}>ADIDAS</NavLink>
+                    </div>
+                    <div>
+                        <NavLink to={'/page2'}>PUMA</NavLink>
+                    </div>
+                    <div>
+                        <NavLink to={'/page3'}>ABIBAS</NavLink>
+                    </div>
+
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path="/" element={<Navigate to={"/page1"} />} />
+
                         <Route path="/page1" element={<PageOne />} />
                         <Route path="/page2" element={<PageTwo />} />
                         <Route path="/page3" element={<PageThree />} />
                         <Route path="/error404" element={<Error404 />} />
+
                         <Route path="/*" element={<Navigate to={"/error404"} />} />
                     </Routes>
                 </div>
