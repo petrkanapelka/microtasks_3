7a102623cd2ec803eb5e332aeeb3add246629eaf
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-25 20:58:56 +0300

    feat: update Puma

--- src/components/pages/Puma.tsx
+++ src/components/pages/Puma.tsx
@@ -1,4 +1,40 @@
 import React from 'react';
+import pumaModel1 from '../../assets/puma/puma1.jpg'
+import pumaModel2 from '../../assets/puma/puma2.jpg'
+import pumaModel3 from '../../assets/puma/puma3.jpg'
+
+export type PumaItem = {
+    id: string;
+    model: string;
+    collection: string;
+    price: string;
+    picture: string;
+}
+
+export const pumaArr: PumaItem[] = [
+    {
+        id: "1",
+        model: 'PUMA RS-X³',
+        collection: 'new collection1',
+        price: '120200$',
+        picture: pumaModel1,
+    },
+    {
+        id: "2",
+        model: 'PUMA Suede Classic',
+        collection: 'new collection22',
+        price: '220300$',
+        picture: pumaModel2
+    },
+    {
+        id: "3",
+        model: 'PUMA Future Rider',
+        collection: 'new collection333',
+        price: '320400$',
+        picture: pumaModel3
+    }
+]
+
 
 export const PageTwo = () => {
     return (
