0350881f929202a3b871bcca044b1d663e4a3270
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-25 20:47:35 +0300

    refactor: rename PageThree

---
+++ src/components/pages/Nike.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const PageThree = () => {
+    return (
+        <div>
+            <h2>Nike Sneakers</h2>
+            <p>
+                Experience the pinnacle of performance and style with Nike sneakers. Renowned for their innovative features and iconic designs, Nike shoes are the choice of athletes and fashion enthusiasts alike. Whether you're hitting the gym or the streets, Nike has the perfect pair to match your lifestyle.
+            </p>
+        </div>
+    );
+};
+
