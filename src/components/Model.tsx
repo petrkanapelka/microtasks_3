c65a5e90c45cee6d9bf774c4a0416ec09eeca603
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-25 21:18:33 +0300

    feat:update Model

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,25 +1,34 @@
 import * as React from 'react';
-import { adidasArr } from './pages/Adidas';
 import { useParams } from 'react-router-dom';
 
-export const Model = () => {
-    let params = useParams();
-    console.log("🚀 ~ Model ~ params ➔", params);
+export type Item = {
+    id: string;
+    model: string;
+    collection: string;
+    price: string;
+    picture: string;
+}
 
-    let sneaker = adidasArr.find((el) => el.id === params.id)
+type ModelProps = {
+    items: Item[];
+}
+
+export const Model: React.FC<ModelProps> = ({ items }) => {
+    let params = useParams();
+    let item = items.find((el) => el.id === params.id);
 
     return (
-        sneaker
+        item
             ? <div
                 style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                <h3> {sneaker.model}</h3 >
+                <h3> {item.model}</h3 >
                 <img
                     style={{ width: '300px', height: 'auto' }}
-                    src={sneaker.picture}
-                    alt={sneaker.model}
+                    src={item.picture}
+                    alt={item.model}
                 />
-                <p>{sneaker.collection}</p>
-                <p>{sneaker.price}</p>
+                <p>{item.collection}</p>
+                <p>{item.price}</p>
             </div >
             : <div>
                 <h3>Model was not found</h3>
