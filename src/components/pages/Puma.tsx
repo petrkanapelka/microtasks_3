bf6f439ece9ee3f9e4533052ef7cdf343a78219e
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-25 20:47:25 +0300

    refactor: rename PageTwo

---
+++ src/components/pages/Puma.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const PageTwo = () => {
+    return (
+        <div>
+            <h2>Puma Sneakers</h2>
+            <p>
+                Step into the world of Puma, where cutting-edge design meets unmatched comfort. Puma sneakers are crafted to provide superior support and durability, making them perfect for everyday wear and athletic activities. Explore our collection and find the perfect pair to elevate your footwear game.
+            </p>
+        </div>
+    );
+};
+
