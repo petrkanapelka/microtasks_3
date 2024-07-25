60e34815b28ab46c16868c2b6b5ecf4af24ef0db
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-25 20:58:41 +0300

    feat: update Nike

--- src/components/pages/Nike.tsx
+++ src/components/pages/Nike.tsx
@@ -1,4 +1,40 @@
 import React from 'react';
+import nikeModel1 from '../../assets/nike/nike1.png'
+import nikeModel2 from '../../assets/nike/nike2.webp'
+import nikeModel3 from '../../assets/nike/nike3.jpg'
+
+export type NikeItem = {
+    id: string;
+    model: string;
+    collection: string;
+    price: string;
+    picture: string;
+}
+
+export const nikeArr: NikeItem[] = [
+    {
+        id: "1",
+        model: 'NIKE Air Max 270',
+        collection: 'new collection1',
+        price: '140200$',
+        picture: nikeModel1,
+    },
+    {
+        id: "2",
+        model: 'NIKE Air Force 1',
+        collection: 'new collection22',
+        price: '240300$',
+        picture: nikeModel2
+    },
+    {
+        id: "3",
+        model: 'NIKE React Infinity Run',
+        collection: 'new collection333',
+        price: '340400$',
+        picture: nikeModel3
+    }
+]
+
 
 export const PageThree = () => {
     return (
