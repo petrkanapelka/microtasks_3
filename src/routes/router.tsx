59b3f937130ce9f844faf0e4ac57b571202e51f7
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-21 20:45:08 +0300

    feat: create router

---
+++ src/routes/router.tsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, } from "react-router-dom";
+import App from "../App";
+import { Error404 } from "../components/pages/Error404";
+import { Adidas } from "../components/pages/Adidas";
+import { Nike } from "../components/pages/Nike";
+import { Puma } from "../components/pages/Puma";
+import { Prices } from "../components/pages/Prices";
+
+export const PATH = {
+    MAIN: '/',
+    ADIDAS: '/adidas',
+    PUMA: '/puma',
+    NIKE: '/nike',
+    ERROR: '/error404',
+    PRICES: '/prices',
+} as const
+
+export const router = createBrowserRouter([
+    {
+        path: PATH.MAIN,
+        element: <App />,
+        errorElement: <Error404 />,
+        children: [
+            {
+                path: PATH.ADIDAS,
+                element: <Adidas />,
+                errorElement: <Error404 />
+            },
+            {
+                path: PATH.PUMA,
+                element: <Puma />,
+                errorElement: <Error404 />
+            },
+            {
+                path: PATH.NIKE,
+                element: <Nike />,
+                errorElement: <Error404 />
+            },
+            {
+                path: PATH.PRICES,
+                element: <Prices />,
+                errorElement: <Error404 />
+            },
+        ]
+    },
+]);
